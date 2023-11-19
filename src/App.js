dd761604f7f12e2c1e754b778896e915154ee120
butch0012 <elnathanmburu@gmail.com>
2023-11-20 01:24:28 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Display from './components/BotDisplay';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import BotCard from "../components/BotCard";
+import React from "react";
+import BotCard from "./BotCard";
 
-// Functional component representing the user's bot army
-export default function YourBotArmy(props) {
-  // Map over the bots in the army to display individual BotCard components
-  const displayBots = props.bots.map(bot => {
-    return <BotCard bot={bot} action={props.action} removeCard={props.removeCard} />;
+// A functional component to display the bot army
+function BotArmy({ bots, removeBot, deleteBot }) {
+  // Map through the bots to create BotCard components for each bot in the army
+  let myBotArmy = bots.map((bot) => {
+    return (
+      <BotCard
+        key={bot.id}
+        bot={bot}
+        deleteBot={removeBot} // Pass the removeBot function to handle bot removal
+        // deleteBot={deleteBot} 
+      />
+    );
   });
 
   return (
-    <div className="ui segment inverted olive bot-army">
-      <div className="ui five column grid">
-        <div className="row bot-army-row">
-          {/* Display the BotCards for the bot army */}
-          {displayBots}
-        </div>
+    <div className="d-block p-2 bg-dark text-white army">
+      {/* Display a heading for the bot army */}
+      <strong>BOT ARMY</strong>
+      <div className="container mt-4">
+        <div className="row">{myBotArmy}</div>
       </div>
     </div>
   );
 }
 
+
+export default BotArmy;
